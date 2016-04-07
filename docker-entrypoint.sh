76c224b327a01150da690828cf126be450d73e2d
Peter Szalatnay <theotherland@gmail.com>
2016-04-07 12:03:13 +0800

    fix copy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -e
 
-if [ ! -d "/usr/share/nginx/html" ]; then
-	mkdir /usr/share/nginx
+if [ ! -d "/usr/share/nginx/html/administrator" ]; then
 	mv /tmp/html /usr/share/nginx
 	chown -Rf nginx.nginx /usr/share/nginx/html
 fi
